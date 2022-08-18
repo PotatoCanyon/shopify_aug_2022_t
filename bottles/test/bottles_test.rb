3354fc0bbfa2429b4889e3629c1e0193438fed09
Sandi Metz <sandi@sandimetz.com>
2022-08-18 14:19:40 -0400

    update tests

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -2,16 +2,18 @@ require_relative '../../test_helper'
 require_relative '../lib/bottles'
 
 class BottlesTest < Minitest::Test
-  def test_the_first_verse
-    expected = "99 bottles of beer on the wall, " +
+  def test_upper_bound_common_rule
+    expected =
+      "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "98 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(99)
   end
 
-  def test_another_verse
-    expected = "3 bottles of beer on the wall, " +
+  def test_lower_bound_common_rule
+    expected =
+      "3 bottles of beer on the wall, " +
       "3 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "2 bottles of beer on the wall.\n"
@@ -19,7 +21,8 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_2
-    expected = "2 bottles of beer on the wall, " +
+    expected =
+      "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "1 bottle of beer on the wall.\n"
@@ -27,7 +30,8 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_1
-    expected = "1 bottle of beer on the wall, " +
+    expected =
+      "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
@@ -35,15 +39,17 @@ class BottlesTest < Minitest::Test
   end
 
   def test_verse_0
-    expected = "No more bottles of beer on the wall, " +
+    expected =
+      "No more bottles of beer on the wall, " +
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(0)
   end
 
-  def test_a_couple_verses
-    expected = "99 bottles of beer on the wall, " +
+  def test_verses
+    expected =
+      "99 bottles of beer on the wall, " +
       "99 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "98 bottles of beer on the wall.\n" +
@@ -55,25 +61,7 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verses(99, 98)
   end
 
-  def test_a_few_verses
-    expected = "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
-      "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
-      "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verses(2, 0)
-  end
-
-  def test_the_whole_song
+  def test_song
     expected = <<~SONG
       99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall.
